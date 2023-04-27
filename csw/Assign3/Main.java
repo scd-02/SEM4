a28b8e1c222d3d5d49eb4749b44c1d73cd83a0a8
Sourav Chandra Dey <100287945+scd-02@users.noreply.github.com>
2023-04-27 16:59:18 +0530

    Update Main.java

--- csw/Assign3/Main.java
+++ csw/Assign3/Main.java
@@ -22,11 +22,18 @@ public class Main {
 //		int a2[] = { 0, 0, 1, 2, 1, 2, 1, 0, 2, 1, 2, 2, 0, 2 };
 //		assign3.Q2_0s1s2sSort.sort0s1s2s(a2);
 
-		int a3[] = { 10,6,7,5,8,5,9 };
-		assign3.Q4_BringAllEleLessThanGivenVal.eleLessThanGivenVal(a3, 8);
+//		int a4[] = { 10,6,7,5,8,9 };
+//		int a4[]= {9,10,6,8,10,5,4};
+//		assign3.Q4_BringAllEleLessThanGivenVal.eleLessThanGivenVal(a4, 8);
+		
+//		int a51[] = {2,1,2,5,7,1,9,3,6,8,8};
+//		int a52[]= {2,1,8,3};
+//		assign3.Q5_SortAccToGivenOrder.sortByOrder(a51, a52);
 		
 		
-
+		int a[]= {2,3,5,6,7,7,9,1,8,10};
+		assign3.Q6_SeparateOddAndEven.eleLessThanGivenVal(a);
+		
 	}
 
 }
